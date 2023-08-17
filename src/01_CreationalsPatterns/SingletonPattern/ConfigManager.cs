b261ff35af10990b13db819035489dca510e3744
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-08-17 07:38:21 +0200

    Dodanie singletona

--- src/01_CreationalsPatterns/SingletonPattern/ConfigManager.cs
+++ src/01_CreationalsPatterns/SingletonPattern/ConfigManager.cs
@@ -8,7 +8,33 @@ namespace SingletonPattern
 {
     public class ConfigManager
     {
+
         private readonly Dictionary<string, object> settings = new Dictionary<string, object>();
+
+        private static object syncLock;
+
+        private static ConfigManager instance;
+
+        public static ConfigManager Instance
+        {
+            get
+            {
+                lock (syncLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        protected ConfigManager()
+        {
+            
+        }
 
         public void Set(string key, object value)
         {
