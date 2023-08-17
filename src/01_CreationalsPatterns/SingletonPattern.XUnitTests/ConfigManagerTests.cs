77c4d38aff706168e9447ec2561e0880b5c88b5d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-08-17 07:38:43 +0200

    Dodanie testu ConfigManagera

--- src/01_CreationalsPatterns/SingletonPattern.XUnitTests/ConfigManagerTests.cs
+++ src/01_CreationalsPatterns/SingletonPattern.XUnitTests/ConfigManagerTests.cs
@@ -6,16 +6,18 @@
         public void SetGet_WhenCalled_ShouldReturnsValue()
         {
             // Arrange
-            ConfigManager manager = new ConfigManager();
+            ConfigManager manager = ConfigManager.Instance;
             manager.Set("name", "Marcin");
 
-            ConfigManager other = new ConfigManager();
+            ConfigManager other = ConfigManager.Instance;
 
             // Act
             object result = other.Get("name");
 
             // Assert
             Assert.Equal("Marcin", result);
+            Assert.True(ReferenceEquals(manager, other));
+            Assert.Same(manager, other);
 
         }
     }
