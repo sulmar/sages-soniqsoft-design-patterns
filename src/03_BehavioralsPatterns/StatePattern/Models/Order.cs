9058fb41f5cd3f5d6b9e170d2e33844516370a7b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-08-19 11:31:38 +0200

    Zastosowanie maszyny stanów skończonych

--- src/03_BehavioralsPatterns/StatePattern/Models/Order.cs
+++ src/03_BehavioralsPatterns/StatePattern/Models/Order.cs
@@ -1,21 +1,57 @@
-﻿using System;
+﻿using Stateless;
+using System;
 using System.Diagnostics.Tracing;
 
 namespace StatePattern
 {
+    // dotnet add package Stateless
+    // https://github.com/dotnet-state-machine/stateless
+
     public class Order
     {
+        private readonly StateMachine<OrderStatus, OrderTrigger> machine;
 
         public Order(OrderStatus initialState = OrderStatus.Placement)
         {
             Id = Guid.NewGuid();
             OrderDate = DateTime.Now;
-            Status = initialState;
+
+            machine = new StateMachine<OrderStatus, OrderTrigger>(initialState);
+
+            machine.Configure(OrderStatus.Placement)
+                .PermitIf(OrderTrigger.Confirm, OrderStatus.Picking, () => IsPaid, "Zapłacono")
+                .IgnoreIf(OrderTrigger.Confirm, () => !IsPaid, "Nie zapłacono")
+                .Permit(OrderTrigger.Cancel, OrderStatus.Canceled);
+
+            machine.Configure(OrderStatus.Picking)
+                .Permit(OrderTrigger.Confirm, OrderStatus.Shipping);
+
+            machine.Configure(OrderStatus.Shipping)
+                .OnEntry(() => Console.WriteLine($"Your order {Id} is ready to send."), "Send email")
+                .Permit(OrderTrigger.Confirm, OrderStatus.Delivered)
+                .OnExit(() => Console.WriteLine($"Your order {Id} was sent."), "Send email");
+
+            machine.Configure(OrderStatus.Delivered)
+                .Permit(OrderTrigger.Confirm, OrderStatus.Completed)
+                .Permit(OrderTrigger.Cancel, OrderStatus.Canceled);
+
+            machine.Configure(OrderStatus.Completed);
+
+            machine.OnTransitioned(transition =>
+            {
+                System.Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{transition.Source} -> {transition.Destination}");
+                System.Console.ResetColor();
+
+            });
         }
+
+        // http://www.webgraphviz.com
+        public string Graph => Stateless.Graph.UmlDotGraph.Format(machine.GetInfo());
 
         public Guid Id { get; set; }
         public DateTime OrderDate { get; set; }
-        public OrderStatus Status { get; private set; }
+        public OrderStatus Status => machine.State;
         public bool IsPaid { get; private set; }
 
         public void Paid()
@@ -23,54 +59,13 @@ namespace StatePattern
             IsPaid = true;
         }
 
-        public void Confirm()
-        {
-            if (Status == OrderStatus.Placement)
-            {
-                if (IsPaid)
-                {
-                    Status = OrderStatus.Picking;
-                }
-            }
-            else if (Status == OrderStatus.Picking)
-            {
-                Status = OrderStatus.Shipping;
-            }
-            else if (Status == OrderStatus.Shipping)
-            {
-                Status = OrderStatus.Delivered;
-            }
-            else if (Status == OrderStatus.Delivered)
-            {
-                Status = OrderStatus.Completed;
-            }
-            else
-            {
-                throw new InvalidOperationException();
-            }
-
-        }
-
-        public void Cancel()
-        {
-            if (Status == OrderStatus.Placement)
-            {
-                Status = OrderStatus.Canceled;
-            }
-            else if (Status == OrderStatus.Delivered)
-            {
-                Status = OrderStatus.Canceled;
-            }
-            else
-            {
-                throw new InvalidOperationException();
-            }
-        }
+        public void Confirm() => machine.Fire(OrderTrigger.Confirm);
+        public void Cancel() => machine.Fire(OrderTrigger.Cancel);
 
         public override string ToString() => $"Order {Id} created on {OrderDate}{Environment.NewLine}";
 
-        public virtual bool CanConfirm => Status == OrderStatus.Placement || Status == OrderStatus.Shipping || Status == OrderStatus.Delivered;
-        public virtual bool CanCancel => Status == OrderStatus.Placement || Status == OrderStatus.Delivered;
+        public virtual bool CanConfirm => machine.CanFire(OrderTrigger.Confirm);
+        public virtual bool CanCancel => machine.CanFire(OrderTrigger.Cancel);
     }
 
     public enum OrderStatus
@@ -88,6 +83,12 @@ namespace StatePattern
         // The customer canceled order
         Canceled
 
+    }
+
+    public enum OrderTrigger
+    {
+        Confirm,
+        Cancel
     }
 
 }
