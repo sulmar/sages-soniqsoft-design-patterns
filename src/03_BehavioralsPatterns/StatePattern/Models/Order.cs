858d374c9a928d6a98029e07d73aa0c1c8885f7f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-08-19 11:04:25 +0200

    Dodanie warunku płatności

--- src/03_BehavioralsPatterns/StatePattern/Models/Order.cs
+++ src/03_BehavioralsPatterns/StatePattern/Models/Order.cs
@@ -6,7 +6,7 @@ namespace StatePattern
     public class Order
     {
 
-        public Order(OrderStatus initialState = OrderStatus.Pending)
+        public Order(OrderStatus initialState = OrderStatus.Placement)
         {            
             Id = Guid.NewGuid();
             OrderDate = DateTime.Now;
@@ -16,14 +16,27 @@ namespace StatePattern
         public Guid Id { get; set; }
         public DateTime OrderDate { get; set; }
         public OrderStatus Status { get; private set; }
+        public bool IsPaid { get; private set; }       
+
+        public void Paid()
+        {
+            IsPaid = true;
+        }
 
         public void Confirm()
         {
-            if (Status == OrderStatus.Pending)
+            if (Status == OrderStatus.Placement)
             {
-                Status = OrderStatus.Sent;
+                if (IsPaid)
+                {
+                    Status = OrderStatus.Picking;
+                }                
             }
-            else if (Status == OrderStatus.Sent)
+            else if (Status == OrderStatus.Picking)
+            {
+                Status = OrderStatus.Shipping;
+            }
+            else if (Status == OrderStatus.Shipping)
             {
                 Status = OrderStatus.Delivered;
             }
@@ -40,13 +53,13 @@ namespace StatePattern
 
         public void Cancel()
         {
-            if (Status == OrderStatus.Pending)
+            if (Status == OrderStatus.Placement)
             {
-                Status = OrderStatus.Cancelled;
+                Status = OrderStatus.Canceled;
             }
             else if (Status == OrderStatus.Delivered)
             {
-                Status = OrderStatus.Cancelled;
+                Status = OrderStatus.Canceled;
             }
             else
             {
@@ -56,17 +69,24 @@ namespace StatePattern
 
         public override string ToString() => $"Order {Id} created on {OrderDate}{Environment.NewLine}";
 
-        public virtual bool CanConfirm => Status == OrderStatus.Pending || Status == OrderStatus.Sent || Status == OrderStatus.Delivered;
-        public virtual bool CanCancel => Status == OrderStatus.Pending || Status == OrderStatus.Delivered;
+        public virtual bool CanConfirm => Status == OrderStatus.Placement || Status == OrderStatus.Shipping || Status == OrderStatus.Delivered;
+        public virtual bool CanCancel => Status == OrderStatus.Placement || Status == OrderStatus.Delivered;
     }
 
     public enum OrderStatus
     {
-        Pending,        
-        Sent,
+        // The customer places an order on the company's website
+        Placement,
+        // The items from the order are picked from the inventory
+        Picking,
+        // The package is handed over to the shipping carrier or courier for delivery to the customer.      
+        Shipping,
+        // The package has been successfully delivered to the customer's specified address.
         Delivered,
+        // The order has been successfully delivered, and the transaction is considered complete.
         Completed,
-        Cancelled
+        // The customer canceled order
+        Canceled
 
     }
 
