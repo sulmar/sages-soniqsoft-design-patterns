a1811fb84cd77816c3bd88a02fae50ab67cf74d4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-08 13:56:11 +0200

    Dodanie testu

--- src/03_BehavioralsPatterns/ChainOfResponsibilityPattern.UnitTests.UnitTests/ProcessMessageTests.cs
+++ src/03_BehavioralsPatterns/ChainOfResponsibilityPattern.UnitTests.UnitTests/ProcessMessageTests.cs
@@ -5,23 +5,43 @@ using System;
 namespace ChainOfResponsibilityPattern.UnitTests.UnitTests
 {
     [TestClass]
-    public class ProcessMessageTests
+    public class FromWhiteListHandlerTests
     {
         [TestMethod]
-        public void ProcessMessage_SenderFromWhiteListWithOrder_ShouldByProcess()
+        public void Handle_SenderFromWhiteListWithOrder_ShouldHandle()
         {
             // Arrange
             string[] whiteList = new string[] { "john@domain.com", "bob@domain.com" };
-            MessageProcessor messageProcessor = new MessageProcessor(whiteList);
-            Message message = new Message { From = "john@domain.com", Title = "Order #1", Body = "Lorem ipsum 953-120-45-91" };
+            var messageHandler = new FromWhiteListHandler(whiteList);
+
+
+            Message message = new Message { From = "john@domain.com" };
 
             // Act
-            var result = messageProcessor.Process(message);
+            var result = messageHandler.Handle(message);
 
-            // Assert
-            Assert.AreEqual("953-120-45-91", result);
+            // Assert            
 
         }
+
+        [TestMethod]
+        [ExpectedExeption(typeof(Exception))]
+        public void Handle_SenderNotFromWhiteListWithOrder_ShouldThrowsException()
+        {
+            // Arrange
+            string[] whiteList = new string[] { "john@domain.com", "bob@domain.com" };
+            var messageHandler = new FromWhiteListHandler(whiteList);
+
+
+            Message message = new Message { From = "a@abc.com" };
+
+            // Act
+            var result = messageHandler.Handle(message);
+
+            // Assert            
+
+        }
+    }
 
         [TestMethod]
         public void ProcessMessage_TitleContainsOrder_ShouldByProcess()
